c3257c147d9bde478b47b1a7fdf34f63540e003c
thinhdoanvu <thinhdv@ntu.edu.vn>
2023-08-03 17:09:43 +0700

    3Aug23

---
+++ Bai2/Controllers/TrangchuController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Bai2.Controllers
+{
+    public class TrangchuController : Controller
+    {
+        // GET: Trangchu
+        public ActionResult Index()
+        {
+            ViewBag.WelcomeString = "Chào mừng đến với ViewBag";
+            return View();
+        }
+    }
+}
