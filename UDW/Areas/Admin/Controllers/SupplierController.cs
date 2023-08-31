f6b8d1386755f36951bd5422996b64f56f0a5ee9
thinhdoanvu <thinhdv@ntu.edu.vn>
2023-08-31 07:47:00 +0700

    Edit_Supplier

--- UDW/Areas/Admin/Controllers/SupplierController.cs
+++ UDW/Areas/Admin/Controllers/SupplierController.cs
@@ -24,21 +24,7 @@ namespace UDW.Areas.Admin.Controllers
         {
             return View(db.Suppliers.ToList());
         }
-        /////////////////////////////////////////////////////////////////////////////////////
-        // GET: Admin/Supplier/Details/5
-        public ActionResult Details(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Suppliers suppliers = db.Suppliers.Find(id);
-            if (suppliers == null)
-            {
-                return HttpNotFound();
-            }
-            return View(suppliers);
-        }
+        
         /////////////////////////////////////////////////////////////////////////////////////
         // GET: Admin/Supplier/Create
         public ActionResult Create()
@@ -103,36 +89,7 @@ namespace UDW.Areas.Admin.Controllers
             return View(suppliers);
         }
 
-        // GET: Admin/Supplier/Edit/5
-        public ActionResult Edit(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Suppliers suppliers = db.Suppliers.Find(id);
-            if (suppliers == null)
-            {
-                return HttpNotFound();
-            }
-            return View(suppliers);
-        }
-
-        // POST: Admin/Supplier/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
-        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "Id,Name,Image,Slug,Order,Fullname,Phone,Email,UrlSite,MetaDesc,MetaKey,CreateBy,CreateAt,UpdateBy,UpdateAt,Status")] Suppliers suppliers)
-        {
-            if (ModelState.IsValid)
-            {
-                db.Entry(suppliers).State = EntityState.Modified;
-                db.SaveChanges();
-                return RedirectToAction("Index");
-            }
-            return View(suppliers);
-        }
+        
 
         // GET: Admin/Supplier/Delete/5
         public ActionResult Delete(int? id)
@@ -160,13 +117,132 @@ namespace UDW.Areas.Admin.Controllers
             return RedirectToAction("Index");
         }
 
-        protected override void Dispose(bool disposing)
+        /////////////////////////////////////////////////////////////////////////////////////
+        // GET: Admin/Supplier/Staus/5:Thay doi trang thai cua mau tin
+        public ActionResult Status(int? id)
         {
-            if (disposing)
+            if (id == null)
             {
-                db.Dispose();
+                //Thong bao that bai
+                TempData["message"] = new XMessage("danger", "Cập nhật trạng thái thất bại");
+                //chuyen huong trang
+                return RedirectToAction("Index", "Supplier");
             }
-            base.Dispose(disposing);
+
+            //khi nhap nut thay doi Status cho mot mau tin
+            Suppliers suppliers  = suppliersDAO.getRow(id);
+            //kiem tra id cua categories co ton tai?
+            if (suppliers == null)
+            {
+                //Thong bao that bai
+                TempData["message"] = new XMessage("danger", "Cập nhật trạng thái thất bại");
+
+                //chuyen huong trang
+                return RedirectToAction("Index", "Category");
+            }
+            //thay doi trang thai Status tu 1 thanh 2 va nguoc lai
+            suppliers.Status = (suppliers.Status == 1) ? 2 : 1;
+
+            //cap nhat gia tri cho UpdateAt/By
+            suppliers.UpdateBy = Convert.ToInt32(Session["UserId"].ToString());
+            suppliers.UpdateAt = DateTime.Now;
+
+            //Goi ham Update trong CategoryDAO
+            suppliersDAO.Update(suppliers);
+
+            //Thong bao thanh cong
+            TempData["message"] = new XMessage("success", "Cập nhật trạng thái thành công");
+
+            //khi cap nhat xong thi chuyen ve Index
+            return RedirectToAction("Index", "Supplier");
+        }
+
+        /////////////////////////////////////////////////////////////////////////////////////
+        // GET: Admin/Supplier/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Suppliers suppliers = db.Suppliers.Find(id);
+            if (suppliers == null)
+            {
+                return HttpNotFound();
+            }
+            return View(suppliers);
+        }
+
+        /////////////////////////////////////////////////////////////////////////////////////
+        // GET: Admin/Supplier/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            ViewBag.OrderList = new SelectList(suppliersDAO.getList("Index"), "Order", "Name");
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Suppliers suppliers = suppliersDAO.getRow(id);
+            if (suppliers == null)
+            {
+                return HttpNotFound();
+            }
+            return View(suppliers);
+        }
+
+        // POST: Admin/Supplier/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Suppliers suppliers)
+        {
+            if (ModelState.IsValid)
+            {
+                //Xu ly cho muc Slug
+                suppliers.Slug = XString.Str_Slug(suppliers.Name);
+                //chuyen doi dua vao truong Name de loai bo dau, khoang cach = dau -
+
+                //Xu ly cho muc Order
+                if (suppliers.Order == null)
+                {
+                    suppliers.Order = 1;
+                }
+                else
+                {
+                    suppliers.Order = suppliers.Order + 1;
+                }
+
+                //xu ly cho phan upload hình ảnh
+                var img = Request.Files["img"];//lay thong tin file
+                if (img.ContentLength != 0)
+                {
+                    string[] FileExtentions = new string[] { ".jpg", ".jpeg", ".png", ".gif" };
+                    //kiem tra tap tin co hay khong
+                    if (FileExtentions.Contains(img.FileName.Substring(img.FileName.LastIndexOf("."))))//lay phan mo rong cua tap tin
+                    {
+                        string slug = suppliers.Slug;
+                        //ten file = Slug + phan mo rong cua tap tin
+                        string imgName = slug + img.FileName.Substring(img.FileName.LastIndexOf("."));
+                        suppliers.Image = imgName;
+                        //upload hinh
+                        string PathDir = "~/Public/img/supplier/";
+                        string PathFile = Path.Combine(Server.MapPath(PathDir), imgName);
+                        img.SaveAs(PathFile);
+                    }
+                }//ket thuc phan upload hinh anh
+
+                //Xu ly cho muc UpdateAt
+                suppliers.UpdateAt = DateTime.Now;
+
+                //Xu ly cho muc UpdateBy
+                suppliers.UpdateBy = Convert.ToInt32(Session["UserId"]);
+
+                suppliersDAO.Update(suppliers);
+
+                //Thong bao thanh cong
+                TempData["message"] = new XMessage("success", "Sửa danh mục thành công");
+                return RedirectToAction("Index");
+            }
+            return View(suppliers);
         }
     }
 }
