9e13eb336bbe5e9785357a402a7b3b5789ea393b
thinhdoanvu <thinhdv@ntu.edu.vn>
2023-09-08 16:47:29 +0700

    menu_Edit

--- UDW/Areas/Admin/Controllers/MenuController.cs
+++ UDW/Areas/Admin/Controllers/MenuController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
 using MyClass.DAO;
@@ -24,12 +25,13 @@ namespace UDW.Areas.Admin.Controllers
         {
             ViewBag.CatList = categoriesDAO.getList("Index");//select * from Categories voi Status !=0
             ViewBag.TopList = topicsDAO.getList("Index");//select * from Topics voi Status !=0
-            ViewBag.PosList = postsDAO.getList("Index");//select * from Posts voi Status !=0
+            ViewBag.PosList = postsDAO.getList("Index","Page");//select * from Posts voi Status !=0
             List<Menus> menu = menusDAO.getList("Index");//select * from Menus voi Status !=0
             return View("Index", menu);//truyen menu duoi dang model
         }
 
-        // POST: Admin/Menu: Select * from
+        /////////////////////////////////////////////////////////////////////////////////////
+        // POST: Admin/Menu/Create
         [HttpPost]
         public ActionResult Index(FormCollection form)
         {
@@ -136,12 +138,142 @@ namespace UDW.Areas.Admin.Controllers
                 }
                 else//check box chưa được nhấn
                 {
-                    TempData["message"] = new XMessage("danger", "Chưa chọn danh mục bài viết");
+                    TempData["message"] = new XMessage("danger", "Chưa chọn danh mục trang đơn");
                 }
             }
 
-            return RedirectToAction("Index", "Menu");
+            //-------------------------Custom------------------------//
+            //Xử lý cho nút ThemCustom ben Index
+            if (!string.IsNullOrEmpty(form["ThemCustom"]))
+            {
+                if (!string.IsNullOrEmpty(form["name"]) && !string.IsNullOrEmpty(form["link"]))
+                //o name, link text được gõ tu phia Index
+                {
+                    //tao ra menu
+                    Menus menus = new Menus();
+                    menus.Name = form["name"];//lay tu o nhap du lieu (form)
+                    menus.Link = form["link"];//lay tu o nhap du lieu (form)
+                    //menu.TableId = post.Id;//vi Table Id allow NULL nen bỏ đi
+                    menus.TypeMenu = "custom";
+                    menus.Position = form["Position"];
+                    menus.ParentId = 0;
+                    menus.Order = 0;
+                    menus.CreateBy = Convert.ToInt32(Session["UserId"].ToString());
+                    menus.CreateAt = DateTime.Now;
+                    menus.Status = 2;//chưa xuất bản
+                    menusDAO.Insert(menus);
+
+                    TempData["message"] = new XMessage("success", "Thêm danh mục thành công");
+                }
+
+                else//check box chưa được nhấn
+                {
+                    TempData["message"] = new XMessage("danger", "Chưa đủ thông tin cho mục tùy chọn Menu");
+                }
+            }
+
+           return RedirectToAction("Index", "Menu");
         }
 
+        /////////////////////////////////////////////////////////////////////////////////////
+        // GET: Admin/Menu/Status
+        public ActionResult Status(int? id)
+        {
+            if (id == null)
+            {
+                TempData["message"] = new XMessage("danger", "Cập nhật không thành công");
+                return RedirectToAction("Index");
+            }
+            Menus menus = menusDAO.getRow(id);
+            if (menus == null)
+            {
+                TempData["message"] = new XMessage("danger", "Cập nhật không thành công");
+                return RedirectToAction("Index");
+            }
+            menus.UpdateAt = DateTime.Now;
+            menus.UpdateBy = Convert.ToInt32(Session["UserId"].ToString());
+            menus.Status = (menus.Status == 1) ? 2 : 1;
+            menusDAO.Update(menus);
+            TempData["message"] = new XMessage("success", "Cập nhật thành công");
+            return RedirectToAction("Index");
+        }
+
+        /////////////////////////////////////////////////////////////////////////////////////
+        // Admin/Menus/Detail: Hien thi mot mau tin
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Menus menus = menusDAO.getRow(id);
+            if (menus == null)
+            {
+                return HttpNotFound();
+            }
+            return View(menus);
+        }
+
+        /////////////////////////////////////////////////////////////////////////////////////
+        // Admin/Menu/Edit: Thay doi mot mau tin
+        public ActionResult Edit(int? id)
+        {
+
+            ViewBag.ParentList = new SelectList(menusDAO.getList("Index"), "Id", "Name");
+            ViewBag.OrderList = new SelectList(menusDAO.getList("Index"), "Order", "Name");
+
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            Menus menus = menusDAO.getRow(id);
+
+            if (menus == null)
+            {
+                return HttpNotFound();
+            }
+            return View("Edit",menus);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Menus menus)
+        {
+            if (ModelState.IsValid)
+            {
+
+                if (menus.ParentId == null)
+                {
+                    menus.ParentId = 0;
+                }
+                if (menus.Order == null)
+                {
+                    menus.Order = 1;
+                }
+                else
+                {
+                    menus.Order += 1;
+                }
+
+                //Xy ly cho muc UpdateAt
+                menus.UpdateAt = DateTime.Now;
+
+                //Xy ly cho muc UpdateBy
+                menus.UpdateBy = Convert.ToInt32(Session["UserId"]);
+
+                //Thong bao thanh cong
+                TempData["message"] = new XMessage("success", "Cập nhật thành công");
+
+                //Cap nhat du lieu
+                menusDAO.Update(menus);
+
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.ParentList = new SelectList(menusDAO.getList("Index"), "Id", "Name");
+            ViewBag.OrderList = new SelectList(menusDAO.getList("Index"), "Order", "Name");
+            return View(menus);
+        }
     }
 }
