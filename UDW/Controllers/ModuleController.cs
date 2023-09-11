fa8f5ec620e23d5fd54ff42248ff93170d32784f
thinhdoanvu <thinhdv@ntu.edu.vn>
2023-09-11 09:07:27 +0700

    Giaodien_menu_Finish

--- UDW/Controllers/ModuleController.cs
+++ UDW/Controllers/ModuleController.cs
@@ -25,12 +25,15 @@ namespace UDW.Controllers
         public ActionResult MainMenuSub(int id)
         {
             List<Menus> list = menusDAO.getListByParentId(id, "MainMenu");
+            //tra ve dong hien hanh cua menu co id = id
+            Menus menus = menusDAO.getRow(id);
             if (list.Count == 0)//menu khong co cap con
             {
-                return View("MainMenuSub_0", list);
+                return View("MainMenuSub_0", menus);
             }
             else//menu co cap con
             {
+                ViewBag.Menu2cap = menus;
                 return View("MainMenuSub_1", list);
             }
             
